edbb1f9bdccd00caa8a7dc1163103682526519f8
Nicklaus <nicholaswakaba@safeboda.com>
2023-12-01 11:25:02 +0300

    Initial Commit

---
+++ Cooperative/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cooperative
+//
+//  Created by Nicholas Wakaba on 01/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
